456ad0001f006a61c4e16148544fe4ec057ce17e
DatariHart <118616414+DatariHart@users.noreply.github.com>
2022-11-24 09:07:35 -0500

    Update list.java

--- list.java
+++ list.java
@@ -3,3 +3,16 @@ Tee
 Hart
 Promo
 Mysteek
+echo "Exam Has Started"
+echo "Type in your name"
+read name
+echo "$name key in your matric number"
+read number
+makdir Repeat
+cd Repeat
+echo "this is a file for repeaters" > Olodos
+if (( $number -ge 5000 ))
+then 
+mv $name Olodos
+else
+echo "congrats $name you have passed"
